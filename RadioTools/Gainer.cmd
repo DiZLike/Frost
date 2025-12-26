42869d3abab970d4c0d3a356f08d3b7c483cff5d
MyHome <diz91@inbox.ru>
2025-12-26 10:10:26 +0300

    Etc.

--- RadioTools/Gainer.cmd
+++ RadioTools/Gainer.cmd
@@ -1,2 +1,2 @@
-..\gainer\bin\Release\net6.0\gainer.exe -gain k -tag c -target -23 "C:\Users\Evgeny\Desktop\MUSIC"
+..\gainer\bin\Release\net6.0\gainer.exe -gain k -tag c -target -23 "C:\Users\diz91\Desktop\MUSIC"
 pause
